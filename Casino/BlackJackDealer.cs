6a6d7207c721f88ea7e3f6bac17d4bdd2ef12a15
Mike-117 <61859863+Mike-117@users.noreply.github.com>
2020-06-22 14:11:19 -0600

    Blackjack update

---
+++ Casino/BlackJackDealer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Casino;
+
+namespace Casino.Blackjack
+{
+    public class BlackJackDealer : Dealer
+    {
+        private List<Card> _hand = new List<Card>();
+        public List<Card> Hand { get { return _hand; } set { _hand = value; } }
+        public bool Stay { get; set; }
+        public bool IsBusted { get; set; }
+
+    }
+}
+        
